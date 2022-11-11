ad83d923fc124cc432523443ad8c746559ffc418
Matthew Harrison <matthewharrison@dyn199021.cc.lehigh.edu>
2022-11-11 15:53:23 -0500

    updated formatting for easier reading

--- digraph_generator.py
+++ digraph_generator.py
@@ -5,16 +5,20 @@ Created on Fri Nov 11 14:48:12 2022
 
 @author: matthewharrison
 """
+
 from graphviz import Digraph
 from bokeh.plotting import figure, show
 from bokeh.palettes import Spectral6
+
 class VersatileDigraph:
     '''Creates a versatile digraph'''
+    
     def __init__(self):
         self.__edge_weights = {}
         self.__node_values = {}
         self.__edge_names = {}
         self.__edge_head = {}
+        
     def add_edge(self,tail, head, **vararg):
         '''Adds an edge to the graph'''
         if isinstance(tail, str) is not True:
@@ -36,6 +40,7 @@ class VersatileDigraph:
             self.__edge_weights[tail][head] = vararg.get('weight',0)
         if vararg.get('weight', 0) >= 0:
             self.__edge_weights[head][tail] = vararg.get('weight',0)
+            
     def add_node(self, node_id, node_value=0):
         '''Add node'''
         if isinstance(node_id, str) is not True:
@@ -48,17 +53,21 @@ class VersatileDigraph:
         self.__edge_weights[node_id] = {}
         self.__edge_names[node_id] = {}
         self.__edge_head[node_id] = {}
+        
     def get_nodes(self):
         '''Get node values'''
         return self.__edge_names.keys()
+    
     def get_edge_wt(self, start, end):
         '''Get an edge weight'''
         return self.__edge_weights[start][end]
+    
     def get_node_value(self, node):
         '''Get node information'''
         if not node in self.get_nodes():
             raise KeyError('Node ' + node + ' is not present in the graph.')
         return self.__node_values[node]
+    
     def print_graph(self):
         '''Print all graph information'''
         for tail in self.get_nodes():
@@ -67,12 +76,15 @@ class VersatileDigraph:
                 print("There is an edge from node " + str(tail) + " to node " + str(head) \
                       + " of weight " + str(self.get_edge_wt(tail,head)) + " and name " \
                           + self.__edge_names[tail][head] + ".")
+                    
     def predecessors(self, node):
         '''Given a node, return a list of nodes that immediately preceed that node'''
         return [n for n in self.get_nodes() if node in self.__edge_names[n]]
+    
     def successors(self, node):
         '''Given a node, return a list of nodes that immediately succeed that node'''
         return list(self.__edge_names[node].keys())
+    
     def successor_on_edge(self, start_node, edge_name):
         '''Given node and edge name, identify successor on named edge'''
         if start_node not in self.__edge_names.keys():
@@ -82,9 +94,11 @@ class VersatileDigraph:
                 if head == start_node:
                     if name == edge_name:
                         print(end_node)
+                        
     def in_degree(self, node):
         '''Return number of edges that lead to a node'''
         return len(self.predecessors(node))
+    
     def out_degree(self, node):
         '''return number of edges that lead from a node'''
         if node not in self.__edge_weights.keys():
@@ -95,6 +109,7 @@ class VersatileDigraph:
                 if head == node:
                     out_nodes.append(tail)
         return len(out_nodes)
+    
     def plot(self):
         '''Plot the digraph'''
         gr = Digraph()
@@ -102,6 +117,7 @@ class VersatileDigraph:
             for tail, weight in tail_dict.items():
                 gr.edge(head,tail,label=str(weight))
         gr.view()
+        
     def edge_weight_plot(self):
         '''Bar graph of edge weights'''
         x_values = []
@@ -116,3 +132,4 @@ class VersatileDigraph:
                color = Spectral6, hover_line_color = 'darkgrey')
         p.xaxis.major_label_orientation = "vertical"
         show(p)
+
